77e7b4eef05c25d494b3d25e306e41954cdb936d
kele-12 <61654062+kele-12@users.noreply.github.com>
2020-03-29 10:41:42 +0800

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System.Linq;
+
+namespace Lx4.txt2
+{
+    delegate void TotalC(int[] a);
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TotalC cb = delegate (int[] x)
+
+            { 
+                Console.WriteLine($"最大值：{x.Max()}");
+            };
+            cb += delegate (int[] x)
+            {
+                Console.WriteLine($"最小值：{x.Min()}");
+            };
+            cb(new[] {78,88,52,66,91});
+
+        }
+    }
+}
